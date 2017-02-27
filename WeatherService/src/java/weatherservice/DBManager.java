411dc4298f9e5817b053ca678ed16d891341c08b
MrDROPTABLE <zachfster@gmail.com>
2017-02-27 13:52:08 -0500

    Create DBManager.java
    This is a Database manager class. Calling the constructor will create a new DBManager will will interact directly with the database. All you need to do is call the methods from the DBManager, just like any other object.

---
+++ WeatherService/src/java/weatherservice/DBManager.java
@@ -0,0 +1,89 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DBManager {
+    
+    
+     Statement st;
+
+    public DBManager() {
+
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error: unable to load driver class!" + ex);
+                System.exit(1);
+            }
+            
+            //Should establish the connection
+            Connection conn = DriverManager.getConnection("jdbc:mysql://pi.cs.oswego.edu:22/" + "tank", "zfoster", "csc380");
+            st = conn.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //Add a users name, password, and email
+    //EMAIL IS THE PRIMARY KEY, this is because it's different for each user
+    public boolean addUser(String name, String password, String email){
+        
+        try{
+            st.executeUpdate("INSERT into Users VALUES('" + name + "', '" + password + "', '" + email + "');");
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+    
+    //We will use this primarily at login.
+    //We search the users by the email and if the password matches, return true (get the okay to log in)
+    public boolean findUser(String password, String email){
+        
+        try{
+            ResultSet user = st.executeQuery("SELECT * FROM Users WHERE Email = '" + email + "' and Password = '" + password + "';");
+            if(user != null){
+                return true;
+            }
+            
+            return false;
+            
+        }catch(Exception e){
+            return false;
+            
+        }
+    }
+    
+    
+    //This is taking the search, and also the user's email
+    //adds the search with the user's email to the "Searches" Table
+    public boolean addSearch(String search, String userEmail){
+        try{
+            st.executeUpdate("INSERT into Searches VALUES('" + search +"', '" + userEmail + "');");
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+    
+    //This returns the history of searches for user when given the user's email
+    public ArrayList<String> getSearches(String userEmail){
+        ArrayList<String> searchesArray = new ArrayList<>();
+        try{
+            ResultSet searches = st.executeQuery("SELECT * FROM Searches WHERE User = '" + userEmail+ "';");
+            while(searches.next()){
+                searchesArray.add(searches.getString("Search"));
+            }
+            return searchesArray;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+}
