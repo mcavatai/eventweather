093fdd0a548521fa0896a169b29a2e19fbc4585a
pk4ever <paulkobe.pk@gmail.com>
2017-03-01 12:54:10 -0500

    Added eventful api jar in an api folder and the eventful class that handles connection and searching in the eventful directory under src.

---
+++ HW2WeatherJSP/src/java/Eventful/Eventful.java
@@ -0,0 +1,44 @@
+import com.evdb.javaapi.APIConfiguration;
+import com.evdb.javaapi.EVDBAPIException;
+import com.evdb.javaapi.EVDBRuntimeException;
+import com.evdb.javaapi.data.Event;
+import com.evdb.javaapi.data.Location;
+import com.evdb.javaapi.data.request.EventSearchRequest;
+import com.evdb.javaapi.operations.EventOperations;
+
+import java.util.List;
+
+/**
+ * Created by paulk4ever on 2/19/17.
+ */
+public class Eventful {
+
+    final private String API_KEY = "qX2dNH9TZGLvBT8B";
+    final private String USER_NAME = "events420";
+    final private String PASSWORD = "pass55word";
+
+    public EventOperations eventOperations;
+    public EventSearchRequest eventSearchRequest;
+    public Location location;
+
+    public Eventful() {
+        APIConfiguration apiConfiguration = new APIConfiguration();
+        apiConfiguration.setApiKey(API_KEY);
+        apiConfiguration.setEvdbUser(USER_NAME);
+        apiConfiguration.setEvdbPassword(PASSWORD);
+
+        eventOperations = new EventOperations();
+        eventSearchRequest = new EventSearchRequest();
+        eventSearchRequest.setPageSize(10);
+        location = new Location();
+        location.setCountry("United Kingdom");
+        eventSearchRequest.setCategory("soccer");
+    }
+
+    public List<Event> search(String keyWord) throws EVDBRuntimeException, EVDBAPIException {
+        eventSearchRequest.setKeywords(keyWord);
+        eventSearchRequest.setLocation(location.getCountry());
+
+        return eventOperations.search(eventSearchRequest).getEvents();
+    }
+}
